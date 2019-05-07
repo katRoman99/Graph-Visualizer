84af8a7346303cadaccaca7db650c600d3d4b28d
katromaniuk <c.romaniuk@lancaster.ac.uk>
2019-05-07 20:39:36 +0100

    Added a class and parameters ready for creating graph 1

--- Graph1.java
+++ Graph1.java
@@ -4,23 +4,28 @@ public class Graph1 extends Graph {
 
     public static void main(String[] args) {
 
-        Node n1 = new Node("A");
-        Node n2 = new Node("B");
-        Node n3 = new Node("C");
-        Node n4 = new Node("D");
+        GameArena arena = new GameArena(800, 800, true);
 
-        Graph g = new Graph();
+        Node n1 = new Node("A", 200, 200, "29FAC4");
+        Node n2 = new Node("B", 350, 200, "29FAC4");
+        Node n3 = new Node("C", 200, 400, "29FAC4");
+        Node n4 = new Node("D", 350, 400, "29FAC4");
+
+        Graph g = new Graph1();
+
+        List<Node> l1 = g.initNodes(n1);
+        List<Node> l2 = g.initNodes(n2);
+        List<Node> l3 = g.initNodes(n3);
+        List<Node> l4 = g.initNodes(n4);
         
-        List<Node> l1 = g.addNodes(n1);
-        List<Node> l2 = g.addNodes(n2);
-        List<Node> l3 = g.addNodes(n3);
-        List<Node> l4 = g.addNodes(n4);
+        g.addEdges(n1, n2, arena);
+        g.addEdges(n1, n3, arena);
+        g.addEdges(n2, n4, arena);
+        g.addEdges(n3, n4, arena);
 
-        g.addEdges(n1, n2);
-        g.addEdges(n1, n3);
-        g.addEdges(n2, n4);
-        g.addEdges(n3, n4);
+        g.addNodes(arena);
 
         g.printGraph();
+        arena.update();
     }
 }
