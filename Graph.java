f2d7c4bb5bfe6b3432d1859c24369066bedae13a
katromaniuk <c.romaniuk@lancaster.ac.uk>
2019-05-07 20:37:27 +0100

    Linked visual representation of the graph with the internal one

--- Graph.java
+++ Graph.java
@@ -2,18 +2,24 @@ import java.util.*;
   
 public class Graph  
 { 
-    private static Map<Node, List<Node>> adjNodes = new HashMap<Node, List<Node>>();
+    static Map<Node, List<Node>> adjNodes = new HashMap<Node, List<Node>>();
 
     static class Node {
-        Object label;
+        String label;
+        Ball ball;
+        Text text;
+        String colour;
 
-        public Node(Object label) {
+        public Node(String label, int x, int y, String colour) {
             this.label = label;
+            this.ball = new Ball(x, y, 30, colour);
+            this.text = new Text(label, x-5, y+5, 20, "BLACK");
+
 
         }
     }
 
-    public List<Node> addNodes(Node source) {
+    public List<Node> initNodes(Node source) {
 
         List<Node> list = new LinkedList<Node>();
         adjNodes.put(source, list);
@@ -21,13 +27,25 @@ public class Graph
     }
     
     //Add a node and create an empty list for it to hold adjacent nodes in the future
-    public void addEdges(Node source, Node mapped) 
+    public void addEdges(Node source, Node mapped, GameArena arena) 
     { 
         List<Node> l = adjNodes.get(source);
         l.add(mapped);
 
         List<Node> l2 = adjNodes.get(mapped);
         l2.add(source);
+
+        Line line = new Line(source.ball.getXPosition(), source.ball.getYPosition(), mapped.ball.getXPosition(), mapped.ball.getYPosition(), 5, "WHITE");
+        arena.addLine(line);
+    }
+
+    public void addNodes(GameArena arena) {
+
+        for (Node n: adjNodes.keySet()) {
+
+            arena.addBall(n.ball);
+            arena.addText(n.text);
+        }
     }
 
     public void printGraph() {
@@ -39,5 +57,4 @@ public class Graph
                 System.out.println("\n");
             }
     }
-       
 }
