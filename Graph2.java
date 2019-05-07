64cc89e91846f8fee87d7d3a737b4384cc2f7149
katromaniuk <c.romaniuk@lancaster.ac.uk>
2019-05-07 20:40:41 +0100

    Added a class and parameters ready for creating graph 2

--- Graph2.java
+++ Graph2.java
@@ -1,38 +1,42 @@
 import java.util.*;
 
-public class Graph2 {
+public class Graph2 extends Graph {
 
     public static void main(String[] args) {
 
-        Node n1 = new Node("E");
-        Node n2 = new Node("F");
-        Node n3 = new Node("G");
-        Node n4 = new Node("H");
-        Node n5 = new Node("I");
+        GameArena arena = new GameArena(800, 800, true);
 
-        Graph g = new Graph();
+        Node n1 = new Node("E", 400, 100, "29FAC4");
+        Node n2 = new Node("F", 600, 200, "29FAC4");
+        Node n3 = new Node("G", 300, 400, "29FAC4");
+        Node n4 = new Node("H", 500, 400, "29FAC4");
+        Node n5 = new Node("I", 200, 200, "29FAC4");
+
+        Graph g = new Graph2();
+
+        List<Node> l1 = g.initNodes(n1);
+        List<Node> l2 = g.initNodes(n2);
+        List<Node> l3 = g.initNodes(n3);
+        List<Node> l4 = g.initNodes(n4);
+        List<Node> l5 = g.initNodes(n5);
+
+        g.addEdges(n1, n2, arena);
+        g.addEdges(n1, n3, arena);
+        g.addEdges(n1, n4, arena);
+        g.addEdges(n1, n5, arena);
+
+        g.addEdges(n2, n3, arena);
+        g.addEdges(n2, n4, arena);
+        g.addEdges(n2, n5, arena);
         
-        List<Node> l1 = g.addNodes(n1);
-        List<Node> l2 = g.addNodes(n2);
-        List<Node> l3 = g.addNodes(n3);
-        List<Node> l4 = g.addNodes(n4);
-        List<Node> l5 = g.addNodes(n5);
+        g.addEdges(n3, n4, arena);
+        g.addEdges(n3, n5, arena);
 
-        g.addEdges(n1, n2);
-        g.addEdges(n1, n3);
-        g.addEdges(n1, n4);
-        g.addEdges(n1, n5);
+        g.addEdges(n4, n5, arena);
 
-        g.addEdges(n2, n3);
-        g.addEdges(n2, n4);
-        g.addEdges(n2, n5);
-        
-        g.addEdges(n3, n4);
-        g.addEdges(n3, n5);
-
-        g.addEdges(n4, n5);
-
+        g.addNodes(arena);
 
         g.printGraph();
+        arena.update();
     }
 }
