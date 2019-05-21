d70cd84180924fd2891b73e6caaeccd4f9912004
katromaniuk <c.romaniuk@lancaster.ac.uk>
2019-05-21 04:45:04 +0100

    Improvements in syntax also added javadoc comments

--- Graph2.java
+++ Graph2.java
@@ -1,33 +1,108 @@
 import java.util.*;
 
+/**
+ * This class builds a simple Graph containing 5 Nodes, creates a seperate GameArena 
+ * and displays it on that Arena.
+ * 
+ * Not suited for building graphs dynamically (yet:) .
+ *
+ * Based on a Graph class.
+ * Instances of the Node class are used here to form the graph.
+ * Instances of a ball class act as a visual representation of a Node.
+ * This class also makes a use of a GameArena class in which the graph is to be drawn.
+ *
+ * @see Graph
+ * @see Node
+ * @see GameArena
+ * @see Ball
+ *
+ */
+
 public class Graph2 extends Graph {
 
-    private Node n1 = new Node("E", 400, 100, "29FAC4");
-    private Node n2 = new Node("F", 600, 200, "29FAC4");
-    private Node n3 = new Node("G", 300, 400, "29FAC4");
-    private Node n4 = new Node("H", 500, 400, "29FAC4");
-    private Node n5 = new Node("I", 200, 200, "29FAC4");
+    //Parameters storing such information as a colour of Nodes and data that each of them will store
+    private String node_col;
+    private String font_col;
+    private String n1_data;
+    private String n2_data;
+    private String n3_data;
+    private String n4_data;
+    private String n5_data;
 
-    private static boolean exit_pressed = false;
-    private static Rectangle exit = new Rectangle(100, 720, 100, 60, "#a97777");
-    private static Text exit_text = new Text("Back", 75, 725, 20, "#ccf4f1");
-    private static Rectangle graph_info = new Rectangle(670, 720, 150, 60, "#a97777");
-    private static Text info_text = new Text("Graph Info", 615, 725, 20, "#ccf4f1");
-    private static Text info = new Text("This is Graph2. \nThis graph is undirected and has 5 nodes.", 100, 600, 20, "#ccf4f1");
+    //Rectangles for normal options such as info and back button
+    private Rectangle exit = new Rectangle(100, 720, 100, 60, "#a97777");
+    private Text exit_text = new Text("Back", 75, 725, 20, "ccf4f1");
+    private Rectangle graph_info = new Rectangle(670, 720, 150, 60, "#a97777");
+    private Text info_text = new Text("Graph Info", 615, 725, 20, "ccf4f1");
 
-    private List<Node> l1 = this.initNodes(n1);
-    private List<Node> l2 = this.initNodes(n2);
-    private List<Node> l3 = this.initNodes(n3);
-    private List<Node> l4 = this.initNodes(n4);
-    private List<Node> l5 = this.initNodes(n5);
+    //Nodes that are used to form this graph
+    private Node n1;
+    private Node n2;
+    private Node n3;
+    private Node n4;
+    private Node n5;
 
-    public Graph2() {
+    //Text containing a vague descrption of this graph
+    private Text general_info;
+
+    //Initializing empty lists to store adjacent nodes
+    private List<Node> l1;
+    private List<Node> l2;
+    private List<Node> l3;
+    private List<Node> l4;
+    private List<Node> l5;
+
+    //All the edges of particular Nodes in a Text form
+    private Text n1_edges;
+    private Text n2_edges;
+    private Text n3_edges;
+    private Text n4_edges;
+    private Text n5_edges;
+
+
+    /**
+     * Constructor. Creates an instance of this Graph2 class which consists of 5 Nodes.
+     * 
+     * @param node_col Colour of the Nodes
+     * @param font_col Colour of the font in which the information will be displayed
+     * @param n1_data Data that Node1 will store
+     * @param n2_data Data that Node2 will store
+     * @param n3_data Data that Node3 will store
+     * @param n4_data Data that Node4 will store
+     * @param n5_data Data that Node5 will store
+     */
+    public Graph2(String node_col, String font_col, String n1_data, String n2_data, String n3_data, String n4_data, String n5_data) {
+        this.node_col = node_col;
+        this.font_col = font_col;
+        this.n1_data = n1_data;
+        this.n2_data = n2_data;
+        this.n3_data = n3_data;
+        this.n4_data = n4_data;
+        this.n5_data = n5_data;
     }
 
+    /**
+     * Method creating a GameArena specifically for this Graph2 class.
+     * Creates both a visual and internal representation of a Graph
+     * by adding edges to vertices and populating the adjacency list.
+     */
     public void showGraph2() {
 
         GameArena arena_graph2 = new GameArena(800, 800);
         arena_graph2.getFrame().setDefaultCloseOperation(arena_graph2.getFrame().DISPOSE_ON_CLOSE);
+
+        //Populating Nodes with necessary data such as String, coordinates and colour
+        n1 = new Node(n1_data, 400, 100, node_col);
+        n2 = new Node(n2_data, 600, 200, node_col);
+        n3 = new Node(n3_data, 300, 400, node_col);
+        n4 = new Node(n4_data, 500, 400, node_col);
+        n5 = new Node(n5_data, 200, 200, node_col);
+
+        l1 = this.initNodes(n1);
+        l2 = this.initNodes(n2);
+        l3 = this.initNodes(n3);
+        l4 = this.initNodes(n4);
+        l5 = this.initNodes(n5);
 
         this.addEdges(n1, n2, arena_graph2, false, "null", 2);
         this.addEdges(n1, n3, arena_graph2, false, "null", 2);
@@ -47,20 +122,22 @@ public class Graph2 extends Graph {
 
         this.addNodes(arena_graph2);
 
-        Text txt1 = new Text(this.getConnections(n1), 80, 600, 20, "#ccf4f1");
-        Text txt2 = new Text(this.getConnections(n2), 80, 600, 20, "#ccf4f1");
-        Text txt3 = new Text(this.getConnections(n3), 80, 600, 20, "#ccf4f1");
-        Text txt4 = new Text(this.getConnections(n4), 80, 600, 20, "#ccf4f1");
-        Text txt5 = new Text(this.getConnections(n5), 80, 600, 20, "#ccf4f1");
+        //Populating the adjacency list
+        n1_edges = new Text(this.getConnections(n1), 80, 600, 20, font_col);
+        n2_edges = new Text(this.getConnections(n2), 80, 600, 20, font_col);
+        n3_edges = new Text(this.getConnections(n3), 80, 600, 20, font_col);
+        n4_edges = new Text(this.getConnections(n4), 80, 600, 20, font_col);
+        n5_edges = new Text(this.getConnections(n5), 80, 600, 20, font_col);
 
-        arena_graph2.addRectangle(exit);
-        arena_graph2.addText(exit_text);
-        arena_graph2.addRectangle(graph_info);
-        arena_graph2.addText(info_text);
+        general_info = new Text("This is Graph2. \nThis graph is undirected and has 5 nodes.", 100, 600, 20, font_col);
+
+        // Adding basic options "buttons" such as information and back button 
+        this.addOptions(arena_graph2, exit, exit_text, graph_info, info_text);
 
         arena_graph2.update();
 
-        while (exit_pressed == false) {
+        //Checking whether any Node or button has been clicked and invoking an appropriate action upon this event.
+        while (true) {
 
             arena_graph2.update();
 
@@ -69,44 +146,55 @@ public class Graph2 extends Graph {
                 this.closeGraph();
             }
             else if (arena_graph2.leftMousePressed() && arena_graph2.objectClicked(graph_info)) {
-                this.removeItems(arena_graph2, txt1, txt2, txt3, txt4, txt5, info);
-                arena_graph2.addText(info);
+                this.removeItems(arena_graph2, n1_edges, n2_edges, n3_edges, n4_edges, n5_edges, general_info);
+                arena_graph2.addText(general_info);
             }
 
             else if (arena_graph2.leftMousePressed() && arena_graph2.objectClicked(n1)) {
-                this.removeItems(arena_graph2, txt1, txt2, txt3, txt4, txt5, info);
-                arena_graph2.addText(txt1);
+                this.removeItems(arena_graph2, n1_edges, n2_edges, n3_edges, n4_edges, n5_edges, general_info);
+                arena_graph2.addText(n1_edges);
             }
 
             else if (arena_graph2.leftMousePressed() && arena_graph2.objectClicked(n2)) {
-                this.removeItems(arena_graph2, txt1, txt2, txt3, txt4, txt5, info);
-                arena_graph2.addText(txt2);
+                this.removeItems(arena_graph2, n1_edges, n2_edges, n3_edges, n4_edges, n5_edges, general_info);
+                arena_graph2.addText(n2_edges);
             }
 
             else if (arena_graph2.leftMousePressed() && arena_graph2.objectClicked(n3)) {
-                this.removeItems(arena_graph2, txt1, txt2, txt3, txt4, txt5, info);
-                arena_graph2.addText(txt3);
+                this.removeItems(arena_graph2, n1_edges, n2_edges, n3_edges, n4_edges, n5_edges, general_info);
+                arena_graph2.addText(n3_edges);
             }
 
             else if (arena_graph2.leftMousePressed() && arena_graph2.objectClicked(n4)) {
-                this.removeItems(arena_graph2, txt1, txt2, txt3, txt4, txt5, info);
-                arena_graph2.addText(txt4);
+                this.removeItems(arena_graph2, n1_edges, n2_edges, n3_edges, n4_edges, n5_edges, general_info);
+                arena_graph2.addText(n4_edges);
             }
 
             else if (arena_graph2.leftMousePressed() && arena_graph2.objectClicked(n5)) {
-                this.removeItems(arena_graph2, txt1, txt2, txt3, txt4, txt5, info);
-                arena_graph2.addText(txt5);
+                this.removeItems(arena_graph2, n1_edges, n2_edges, n3_edges, n4_edges, n5_edges, general_info);
+                arena_graph2.addText(n5_edges);
             }
         }
     }
 
-    public void removeItems(GameArena arena, Text t1, Text t2, Text t3, Text t4, Text t5, Text i) {
+    /**
+     * A method that removes all the Text information displayed on the screen when necessary.
+     * 
+     * @param arena Arena on which the information is displayed
+     * @param n1_edges Text representation of the adjacency list for Node1
+     * @param n2_edges Text representation of the adjacency list for Node2
+     * @param n3_edges Text representation of the adjacency list for Node3
+     * @param n4_edges Text representation of the adjacency list for Node4
+     * @param n5_edges Text representation of the adjacency list for Node5
+     * @param general_info Text representation of the descrption of the graph
+     */
+    public void removeItems(GameArena arena, Text n1_edges, Text n2_edges, Text n3_edges, Text n4_edges, Text n5_edges, Text general_info) {
 
-        arena.removeText(t1);
-        arena.removeText(t2);
-        arena.removeText(t3);
-        arena.removeText(t4);
-        arena.removeText(t5);
-        arena.removeText(i);
+        arena.removeText(n1_edges);
+        arena.removeText(n2_edges);
+        arena.removeText(n3_edges);
+        arena.removeText(n4_edges);
+        arena.removeText(n5_edges);
+        arena.removeText(general_info);
     }
 }
--- Graph4.java
+++ Graph4.java
@@ -1,36 +1,106 @@
 import java.util.*;
 
+/**
+ * This class builds a simple directed Graph containing 4 Nodes, creates a seperate GameArena 
+ * and displays it on that Arena.
+ * 
+ * Not suited for building graphs dynamically (yet:) .
+ *
+ * Based on a Graph class.
+ * Instances of the Node class are used here to form the graph.
+ * Instances of a ball class act as a visual representation of a Node.
+ * This class also makes a use of a GameArena class in which the graph is to be drawn.
+ *
+ * @see Graph
+ * @see Node
+ * @see GameArena
+ * @see Ball
+ *
+ */
+
 public class Graph4 extends Graph {
 
+    //Parameters storing such information as a colour of Nodes and data that each of them will store
+    private String node_col;
+    private String font_col;
+    private String n1_data;
+    private String n2_data;
+    private String n3_data;
+    private String n4_data;
+
+    //Lines which act as arrows on self-arcs
     private Line h1 = new Line(231, 426, 224, 422, 4, "WHITE");
     private Line h2 = new Line(231, 426, 223, 434, 4, "WHITE");
     private Line h3 = new Line(569, 426, 576, 422, 4, "WHITE");
     private Line h4 = new Line(569, 426, 575, 434, 4, "WHITE");
 
-    private Node n1 = new Node("J", 400, 200, "29FAC4");
-    private Node n2 = new Node("K", 250, 400, "29FAC4");
-    private Node n3 = new Node("L", 400, 600, "29FAC4");
-    private Node n4 = new Node("M", 550, 400, "29FAC4");
+    //Rectangles for normal options such as info and back button
+    private Rectangle exit = new Rectangle(100, 720, 100, 60, "#a97777");
+    private Text exit_text = new Text("Back", 75, 725, 20, "ccf4f1");
+    private Rectangle graph_info = new Rectangle(670, 720, 150, 60, "#a97777");
+    private Text info_text = new Text("Graph Info", 615, 725, 20, "ccf4f1");
 
-    private static boolean exit_pressed = false;
-    private static Rectangle exit = new Rectangle(100, 720, 100, 60, "#a97777");
-    private static Text exit_text = new Text("Back", 75, 725, 20, "#ccf4f1");
-    private static Rectangle graph_info = new Rectangle(670, 720, 150, 60, "#a97777");
-    private static Text info_text = new Text("Graph Info", 615, 725, 20, "#ccf4f1");
-    private static Text info = new Text("This is Graph4. \nThis graph is directed and has 4 nodes.", 100, 100, 20, "#ccf4f1");
+    //Nodes that are used to form this graph
+    private Node n1;
+    private Node n2;
+    private Node n3;
+    private Node n4;
 
-    private List<Node> l1 = this.initNodes(n1);
-    private List<Node> l2 = this.initNodes(n2);
-    private List<Node> l3 = this.initNodes(n3);
-    private List<Node> l4 = this.initNodes(n4);
+    //All the edges of particular Nodes in a Text form
+    private Text n1_edges;
+    private Text n2_edges;
+    private Text n3_edges;
+    private Text n4_edges;
 
-    public Graph4() {
+    //Text containing a vague descrption of this graph
+    private Text general_info;
+
+    //Initializing empty lists to store adjacent nodes
+    private List<Node> l1;
+    private List<Node> l2;
+    private List<Node> l3;
+    private List<Node> l4;
+
+     /**
+     * Constructor. Creates an instance of this Graph4 class which consists of 4 Nodes and is directed.
+     * 
+     * @param node_col Colour of the Nodes
+     * @param font_col Colour of the font in which the information will be displayed
+     * @param n1_data Data that Node1 will store
+     * @param n2_data Data that Node2 will store
+     * @param n3_data Data that Node3 will store
+     * @param n4_data Data that Node4 will store
+     */
+
+    public Graph4(String node_col, String font_col, String n1_data, String n2_data, String n3_data, String n4_data) {
+        this.node_col = node_col;
+        this.font_col = font_col;
+        this.n1_data = n1_data;
+        this.n2_data = n2_data;
+        this.n3_data = n3_data;
+        this.n4_data = n4_data;
     }
 
+    /**
+     * Method creating a GameArena specifically for this Graph4 class.
+     * Creates both a visual and internal representation of a Graph
+     * by adding edges to vertices and populating the adjacency list.
+     */
     public void showGraph4() {
 
         GameArena arena_graph4 = new GameArena(800, 800);
         arena_graph4.getFrame().setDefaultCloseOperation(arena_graph4.getFrame().DISPOSE_ON_CLOSE);
+
+        //Populating Nodes with necessary data such as String, coordinates and colour
+        n1 = new Node(n1_data, 400, 200, node_col);
+        n2 = new Node(n2_data, 250, 400, node_col);
+        n3 = new Node(n3_data, 400, 600, node_col);
+        n4 = new Node(n4_data, 550, 400, node_col);
+
+        l1 = this.initNodes(n1);
+        l2 = this.initNodes(n2);
+        l3 = this.initNodes(n3);
+        l4 = this.initNodes(n4);
 
         this.addEdges(n1, n2, arena_graph4, true, "null", 87);
         this.addEdges(n2, n3, arena_graph4, true, "null", 87);
@@ -40,27 +110,30 @@ public class Graph4 extends Graph {
         this.addEdges(n4, n1, arena_graph4, true, "null", 87);
         this.addEdges(n1, n4, arena_graph4, true, "null", 87);
 
+        //Populating the adjacency list
+        n1_edges = new Text(this.getConnections(n1), 80, 100, 20, font_col);
+        n2_edges = new Text(this.getConnections(n2), 80, 100, 20, font_col);
+        n3_edges = new Text(this.getConnections(n3), 80, 100, 20, font_col);
+        n4_edges = new Text(this.getConnections(n4), 80, 100, 20, font_col);
+
+        general_info = new Text("This is Graph4. \nThis graph is directed and has 4 nodes.", 100, 100, 20, font_col);
+
         this.addNodes(arena_graph4);
 
-        Text txt1 = new Text(this.getConnections(n1), 80, 100, 20, "#ccf4f1");
-        Text txt2 = new Text(this.getConnections(n2), 80, 100, 20, "#ccf4f1");
-        Text txt3 = new Text(this.getConnections(n3), 80, 100, 20, "#ccf4f1");
-        Text txt4 = new Text(this.getConnections(n4), 80, 100, 20, "#ccf4f1");
-
-
-        arena_graph4.addRectangle(exit);
-        arena_graph4.addText(exit_text);
-        arena_graph4.addRectangle(graph_info);
-        arena_graph4.addText(info_text);
+        // Adding basic options "buttons" such as information and back button 
+        this.addOptions(arena_graph4, exit, exit_text, graph_info, info_text);
 
         this.printGraph();
+
+        //Adding Arrows(Lines) for vertices with self-arcs
         arena_graph4.addLine(h1);
         arena_graph4.addLine(h2);
         arena_graph4.addLine(h3);
         arena_graph4.addLine(h4);
         arena_graph4.update();
 
-        while (exit_pressed == false) {
+        //Checking whether any Node or button has been clicked and invoking an appropriate action upon this event.
+        while (true) {
             arena_graph4.update();
 
             if (arena_graph4.leftMousePressed() && arena_graph4.objectClicked(exit)) {
@@ -68,37 +141,28 @@ public class Graph4 extends Graph {
                 this.closeGraph();
             }
             else if (arena_graph4.leftMousePressed() && arena_graph4.objectClicked(graph_info)) {
-                this.removeItems(arena_graph4, txt1, txt2, txt3, txt4, info);
-                arena_graph4.addText(info);
+                this.removeItems134(arena_graph4, n1_edges, n2_edges, n3_edges, n4_edges, general_info);
+                arena_graph4.addText(general_info);
             }
             else if (arena_graph4.leftMousePressed() && arena_graph4.objectClicked(n1)) {
-                this.removeItems(arena_graph4, txt1, txt2, txt3, txt4, info);
-                arena_graph4.addText(txt1);
+                this.removeItems134(arena_graph4, n1_edges, n2_edges, n3_edges, n4_edges, general_info);
+                arena_graph4.addText(n1_edges);
             }
 
             else if (arena_graph4.leftMousePressed() && arena_graph4.objectClicked(n2)) {
-                this.removeItems(arena_graph4, txt1, txt2, txt3, txt4, info);
-                arena_graph4.addText(txt2);
+                this.removeItems134(arena_graph4, n1_edges, n2_edges, n3_edges, n4_edges, general_info);
+                arena_graph4.addText(n2_edges);
             }
 
             else if (arena_graph4.leftMousePressed() && arena_graph4.objectClicked(n3)) {
-                this.removeItems(arena_graph4, txt1, txt2, txt3, txt4, info);
-                arena_graph4.addText(txt3);
+                this.removeItems134(arena_graph4, n1_edges, n2_edges, n3_edges, n4_edges, general_info);
+                arena_graph4.addText(n3_edges);
             }
 
             else if (arena_graph4.leftMousePressed() && arena_graph4.objectClicked(n4)) {
-                this.removeItems(arena_graph4, txt1, txt2, txt3, txt4, info);
-                arena_graph4.addText(txt4);
+                this.removeItems134(arena_graph4, n1_edges, n2_edges, n3_edges, n4_edges, general_info);
+                arena_graph4.addText(n4_edges);
             }
         }
-    }
-
-    public void removeItems(GameArena arena, Text t1, Text t2, Text t3, Text t4, Text i) {
-
-        arena.removeText(t1);
-        arena.removeText(t2);
-        arena.removeText(t3);
-        arena.removeText(t4);
-        arena.removeText(i);
     }
 }
