38d6336032a5b0f592f907381ec02a7708e8a8aa
katromaniuk <c.romaniuk@lancaster.ac.uk>
2019-05-21 04:43:32 +0100

    Added javadoc comments

--- MainScreen.java
+++ MainScreen.java
@@ -1,18 +1,9 @@
+/**
+ * Main class that acts as an intermediate between the StartScreen and SelectionScreen.
+ * Handles the execution of the whole application.
+ */
+
 public class MainScreen {
-
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = 800;
-    private static final int REC_W = 200;
-    private static final int REC_H = 100;
-    private static final int FONT_SIZE = 40;
-
-    private static Object arena_object;
-
-    private static Rectangle rec1 = new Rectangle(WIDTH/5+10, HEIGHT/2.5, REC_W, REC_H, "#a97777");
-    private static Rectangle rec2 = new Rectangle(WIDTH/5 + REC_W+40, HEIGHT/2.5, REC_W, REC_H, "#a97777");
-    private static Rectangle rec3 = new Rectangle(WIDTH/5 + 2*REC_W+70, HEIGHT/2.5, REC_W, REC_H, "#a97777");
-    private static Rectangle rec4 = new Rectangle(WIDTH/5+10, HEIGHT/1.7, REC_W, REC_H, "#a97777");
-    private static Rectangle rec5 = new Rectangle(WIDTH/5 + REC_W+40, HEIGHT/1.7, REC_W, REC_H, "#a97777");
 
     private static boolean mouse = true;
 
